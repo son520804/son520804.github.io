a9a9f1fdac6b46b898dc7b28e25d6800c663c82c
In Son Zeng <michiganboy@IndeMacBook-Pro.local>
2021-05-01 15:08:22 -0400

    styling

---
+++ about/online-store/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Welcome!</h1>
+    {{products}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  computed:{
+    products: function(){
+      return this.$Store.state.products
+    }
+  }
+};
+</script>
